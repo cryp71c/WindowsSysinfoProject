ddfeb67e45ab5db570480b90fe101fbef7fef703
Alex <>
2024-09-10 22:24:47 -0500

    This was a test in a vM weird memory returns, will check it again on a non-virtual box to test my code.

--- Sysinfomain.cpp
+++ Sysinfomain.cpp
@@ -3,8 +3,27 @@
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
+#include <fileapi.h>
+#include <psapi.h>
+#include <ntverp.h>
+
+char* getAvailableDrives(unsigned int bitmask) {
+    const int NUM_DRIVES = 26;  // Assuming drives from A to Z
+    char* available_drives = (char*)malloc(NUM_DRIVES + 1);
+	int index=0;
+	for (int i = 0; i < NUM_DRIVES; ++i) {
+        // Check if the ith bit is set
+        if (bitmask & (1 << i)) {
+       	    // Print the corresponding drive letter
+            available_drives[index++]='A'+i;
+        }
+    }
+    return(available_drives);
+}
 
 int main(){
+	SYSTEMTIME local_time;
+	GetLocalTime(&local_time);
 	
 	char buf[256];
 	DWORD bufSize = sizeof(buf);
@@ -22,12 +41,11 @@ int main(){
 		sysError << "[*] Memory allocation failed.";
 		return 1;
 	}
-	
 	for(size_t i = 0; i < len; i++){
 		computerName[i] = buf[i];
 	}
 	
-	// Get Processor Type
+	// Get Processor Architecture
 	SYSTEM_INFO myInfo;
 	GetSystemInfo(&myInfo);
 	const char *arch = nullptr;
@@ -54,6 +72,8 @@ int main(){
     		arch="UNKNOWN";
             break;
 	}
+	
+	// Get Type of processor
 	DWORD processorType = myInfo.dwProcessorType;
 	const char *pType = nullptr;
 	switch(processorType){
@@ -77,10 +97,31 @@ int main(){
             break;
 	}
 	
+	// Get Logical Drives
+	DWORD drive_bitmask = GetLogicalDrives();
+	char *available_drives;
+	available_drives = getAvailableDrives(drive_bitmask);
+	
+	
+	// Get installed memory
+	MEMORYSTATUSEX  system_ram;
+	GlobalMemoryStatusEx(&system_ram);
+	
+	char date_time[23]; // 18 characters + 5 extra bytes
+	snprintf(date_time, sizeof(date_time), "%02d:%02d:%02d %02d%02d%04d",
+		local_time.wHour, local_time.wMinute, local_time.wSecond,
+		local_time.wDay, local_time.wMonth, local_time.wYear);
+	
 	std::ofstream sysInfo("sysInfo.txt");
+	sysInfo << "-- Run At " << date_time << " --\n";
 	sysInfo << "Computer Name: " << computerName << "\n";
 	sysInfo << "Processor Type: " << pType << "\n";
-	sysInfo << "Processor: " << arch;
+	sysInfo << "Processor: " << arch << "\n";
+	sysInfo << "Available Mem: " << system_ram.ullAvailPhys << "\n";
+	sysInfo << "Total Mem: " << system_ram.ullTotalPhys << "\n"; // Get installed memory in KB and devide it by 24 into MB
+	sysInfo << "Available Drives: "<< available_drives << "\n";
+	
+	
 	free(computerName);
 	return 0;
 }
